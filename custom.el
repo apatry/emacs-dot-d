59e4f11834c53b0a0d150765b3c83397788256ab
Alexandre Patry <apatry@linkedin.com>
2017-11-20 15:00:56 -0800

    Add support for javascript

--- custom.el
+++ custom.el
@@ -6,10 +6,10 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(org-agenda-files (quote ("~/Documents/plates/agora.org")))
  '(package-selected-packages
    (quote
-    (sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+))))
+    (sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+)))
+ '(org-agenda-files (quote ("~/Documents/newworld.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
