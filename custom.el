7f0924edfcc5f7db2c5d69ca7b7ebe7aadc4a57c
Alexandre Patry <alex@nlpfu.com>
2019-09-28 23:09:47 -0700

    Configure projectile

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(org-agenda-files
    (quote
-    ("~/Documents/org/daily.org" "~/Documents/newworld.org")))
+    ("~/Documents/today.org" "~/Documents/org/daily.org" "~/Documents/newworld.org")))
  '(package-selected-packages
    (quote
     (ox-hugo sparql-mode nix-mode helm helm-config auctex sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+))))
