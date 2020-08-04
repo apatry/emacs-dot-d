62d1b4f96b387e008e0bb6b16ab5156f83f26eb8
Alexandre Patry <apatry@linkedin.com>
2020-08-04 08:43:56 -0700

    Enable auto-fill-mode in org-mode by default
    Also add a new file to my org-agenda to track project.

--- custom.el
+++ custom.el
@@ -6,7 +6,9 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(org-agenda-files (quote ("~/Documents/today.org")))
+ '(org-agenda-files
+   (quote
+    ("~/org/projects.org" "~/Documents/communications.org" "~/Documents/today.org")))
  '(package-selected-packages
    (quote
     (org-roam python-black org ox-hugo sparql-mode nix-mode helm helm-config auctex sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+)))
