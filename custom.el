68d7bc5c7f0adb38633d9799e77cce919f923c35
Alexandre Patry <apatry@linkedin.com>
2020-08-28 19:33:01 -0700

    Update config for emacs 27

--- custom.el
+++ custom.el
@@ -4,14 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(org-agenda-files
-   (quote
-    ("~/org/projects.org" "~/Documents/communications.org" "~/Documents/today.org")))
+   '("~/org/projects.org" "~/Documents/communications.org" "~/Documents/today.org"))
  '(package-selected-packages
-   (quote
-    (org-ref org-roam python-black org ox-hugo sparql-mode nix-mode helm helm-config auctex sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+)))
+   '(org-ref org-roam python-black org ox-hugo sparql-mode nix-mode helm helm-config auctex sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+))
  '(pdf-tools-handle-upgrades nil)
  '(python-indent-guess-indent-offset nil)
  '(python-indent-offset 2))
