f91572ee97f5ba74e620d442d33649cd1ca97f7a
Alex Patry <alex@nlpfu.com>
2021-02-15 16:31:52 -0800

    Start ssh-agent when starting exwm
    Also add the default ssh key on startup. This will prompt for a
    password at the start of the session.

--- exwm/exwm-start.sh
+++ exwm/exwm-start.sh
@@ -5,5 +5,9 @@ picom &
 # Enable screen locking on suspend
 xss-lock -- slock &
 
+# Start ssh-agent so that we don't have to type the passphrase all the time
+eval `ssh-agent`
+ssh-add &
+
 # Start exwm
 exec dbus-launch --exit-with-session emacs -mm --debug-init -l /home/alex/.emacs.d/exwm/exwm.el
