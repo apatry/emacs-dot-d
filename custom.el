00e9acceabe28c94bc44e0064ddfb0d39cfc4834
Alex Patry <alex@nlpfu.com>
2021-02-27 20:16:07 -0800

    Add a mu4e bookmark for flagged messages
    I will use this bookmark for emails I need to follow-up on.

--- custom.el
+++ custom.el
@@ -4,17 +4,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(org-agenda-files
-   '("~/org/projects.org" "~/Documents/communications.org" "~/Documents/today.org"))
+   (quote
+    ("~/org/projects.org" "~/Documents/communications.org" "~/Documents/today.org")))
  '(package-selected-packages
-   '(esup org-journal epresent latex py-isort ivy-hydra org-ref org-roam python-black org ox-hugo sparql-mode nix-mode helm helm-config auctex sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+))
+   (quote
+    (esup org-journal epresent latex py-isort ivy-hydra org-ref org-roam python-black org ox-hugo sparql-mode nix-mode helm helm-config auctex sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+)))
  '(pdf-tools-handle-upgrades nil)
  '(python-indent-guess-indent-offset nil)
  '(safe-local-variable-values
-   '((epresent-text-scale . 400)
+   (quote
+    ((epresent-text-scale . 400)
      (epresent-text-scale . 200)
-     (epresent-frame-level . 2))))
+     (epresent-frame-level . 2)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
