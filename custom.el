909ce4542f3bb61e408d3f6e637101a5dbeecbe7
Alex Patry <patryale@amazon.com>
2026-05-03 11:11:53 -0700

    Replace Ivy/Counsel with Vertico/Consult and add gptel, smooth scrolling
    - Switch completion framework from Ivy/Counsel to Vertico + Orderless + Prescient
    - Replace counsel with consult (buffer, recent-file, yank-pop, goto-line, search)
    - Add consult-org-roam for roam search and backlinks
    - Add gptel with Bedrock backend (Claude Sonnet) for LLM integration
    - Add ultra-scroll for smooth scrolling
    - Set JetBrainsMono Nerd Font as default font
    - Improve org-journal: enable agenda integration and carryover of open tasks
    - Add org-roam file rename helper and use sqlite-builtin connector
    - Simplify org-capture templates and add refile targets
    - Expand package list and normalize indentation throughout

--- custom.el
+++ custom.el
@@ -4,15 +4,36 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
- '(org-agenda-files '("~/org/today.org" "/Users/patryale/Documents/today.org"))
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
+     "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e"
+     default))
+ '(org-agenda-files
+   '("/Users/patryale/org/today.org" "/Users/patryale/journal/20260501"))
  '(package-selected-packages
-   '(emacsql-sqlite async async-shell-command-buffer exec-path-from-shell lsp-pyright esup org-journal epresent latex py-isort ivy-hydra org-ref org-roam python-black org ox-hugo sparql-mode nix-mode helm helm-config auctex sphinx-mode sphinx-doc pig-mode solarized-theme magit groovy-mode graphviz-dot-mode gradle-mode git-gutter+))
+   '(all-the-icons async auctex auto-complete-nxml auto-package-update
+                   cider company consult-org-roam consult-projectile
+                   counsel-projectile doom-modeline doom-themes
+                   epresent esup exec-path-from-shell flycheck
+                   git-gutter gnuplot gnuplot-mode gptel
+                   graphviz-dot-mode groovy-mode ivy-bibtex
+                   ivy-explorer ivy-hydra jinja2-mode julia-repl lispy
+                   lsp-ivy lsp-java lsp-julia lsp-pyright lsp-ui magit
+                   nix-mode orderless org-bullets org-fragtog
+                   org-journal org-noter org-ql org-ref ox-hugo
+                   pandoc-mode pdf-tools pig-mode plantuml-mode poetry
+                   prescient protobuf-mode py-isort python-black
+                   python-docstring rainbow-delimiters rnc-mode
+                   rust-playground rustic scala-mode sparql-mode
+                   thrift toml-mode typescript-mode ultra-scroll
+                   vertico vertico-prescient visual-fill-column
+                   which-key))
+ '(package-vc-selected-packages
+   '((ultra-scroll :vc-backend Git :url
+                   "https://github.com/jdtsmith/ultra-scroll")))
  '(pdf-tools-handle-upgrades nil)
  '(python-indent-guess-indent-offset nil)
  '(safe-local-variable-values
-   '((epresent-text-scale . 400)
-     (epresent-text-scale . 200)
+   '((epresent-text-scale . 400) (epresent-text-scale . 200)
      (epresent-frame-level . 2))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- init.el
+++ init.el
@@ -48,3 +48,16 @@
       (message "Skip tangling of %s, reusing %s directly." source compiled)
       (load compiled))))
 (put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-vc-selected-packages
+   '((ultra-scroll :vc-backend Git :url "https://github.com/jdtsmith/ultra-scroll"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
